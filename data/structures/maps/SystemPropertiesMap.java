f0c963bbced31a700c564d3831c530ff36221c37
Doug Hoff <email@doughoff.com>
2017-03-24 11:21:04 -0500

    class update

--- data/structures/maps/SystemPropertiesMap.java
+++ data/structures/maps/SystemPropertiesMap.java
@@ -3,14 +3,28 @@ package data.structures.maps;
 import java.util.*;
 
 public class SystemPropertiesMap {
+	private static void print(Map<String,String> systemProperties) {
+		System.out.println("---------------------------------------");
+		for (Map.Entry<String,String> pair : systemProperties.entrySet()) {
+			System.out.printf("%-30s = %s\n", pair.getKey(), pair.getValue());
+		}
+	}
 	public static void main(String[] args) {
 		@SuppressWarnings({ "unchecked", "rawtypes" })
-		SortedMap<String, String> systemProperties 
-			= new TreeMap(System.getProperties());
+		SortedMap<String, String> systemProperties = new TreeMap(System.getProperties());
+		print(systemProperties);
 		
+		// print only Java runtime and specification info
 		SortedMap<String, String> systemPropertiesSubMap = 
-				systemProperties.subMap("java.runtime.name", "java.vm.info");
+				systemProperties.subMap("java.runtime.name", "java.vendor");
+		print(systemPropertiesSubMap);
 		
-		System.out.println(systemPropertiesSubMap);
+		// print only user properties
+		SortedMap<String, String> userPropertiesSubMap = 
+				systemProperties.tailMap("user.country");
+		print(userPropertiesSubMap);
+		
 	}
+
+
 }
