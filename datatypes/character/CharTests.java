6f5e02d09896ddc74d95b2d4b02b22e7bf48cdfe
Doug Hoff <email@doughoff.com>
2018-05-21 10:07:20 -0500

    update

---
+++ datatypes/character/CharTests.java
@@ -0,0 +1,15 @@
+package datatypes.character;
+
+public class CharTests {
+
+	public static void main(String[] args) {
+		System.out.println('A');
+		System.out.println(0 +'A');
+		System.out.println( (char)(1 + 'A') );
+		System.out.println( 'A' + 1);
+		System.out.println('A' + 'B' + 'C');
+		System.out.println("A" + "B" + "C");
+		System.out.println("Alphabet: " + 'A' + 'B' + 'C');
+	}
+
+}
