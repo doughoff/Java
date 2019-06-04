c34b2b95443a21a76e74aa5ec43124454503addc
doughoff <email@doughoff.com>
2019-06-04 15:55:50 -0500

    update

---
+++ oo/ClassTemplate.java
@@ -0,0 +1,17 @@
+package oo;
+
+public class ClassTemplate {
+	//-------------------------------------- data structure
+	//----------constants
+	//----------instance variables (fields)
+	//----------static variables
+	
+	//---------------------------------------methods
+	//---------  constructors
+	//---------  gets and sets
+	//---------- instance methods
+	//---------- static methods
+	
+	//--------------------------------------- test (main)
+	
+}
