f4c825fd113c89d01dffd541aec98d6e81529362
doughoff <email@doughoff.com>
2019-06-06 08:57:35 -0500

    new

---
+++ projectEuler/Problem1.java
@@ -0,0 +1,41 @@
+package projectEuler;
+
+public class Problem1 {
+	// https://projecteuler.net/problem=1
+	
+	/* If we list all the natural numbers below 10 
+	 * that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+	 * The sum of these multiples is 23. 
+	 * Find the sum of all the multiples of 3 or 5 below 1000.
+	*/
+	private int sum;
+	private int candidate;
+	
+	public static int maxExclusiveRange = 1000;
+	
+	public boolean candidateIsMultipleOf3Or5() {
+		boolean isMultipleOf3 = this.candidate % 3 == 0;
+		boolean isMultipleOf5 = this.candidate % 5 == 0;
+		boolean result = isMultipleOf3 || isMultipleOf5;
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Problem1 p1 = new Problem1();
+		
+		// calculate sum
+		for (p1.candidate = 0; p1.candidate < maxExclusiveRange; p1.candidate++) {
+			if (p1.candidateIsMultipleOf3Or5()) {
+				p1.sum += p1.candidate;
+			}
+		}
+		// print sum
+		printSum(p1);
+		
+	}
+
+	private static void printSum(Problem1 p1) {
+		System.out.printf("The sum of all multiples of 3 or 5 "
+				+ "below %d is %,d.\n", maxExclusiveRange, p1.sum);
+	}
+}
