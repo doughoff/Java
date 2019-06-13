bb2adff7f8794889350aecdd036ae28b2c5f93dd
doughoff <email@doughoff.com>
2019-06-13 16:15:24 -0500

    update

---
+++ oo/basic/pseudo/NumberPair.java
@@ -0,0 +1,24 @@
+package oo.basic.pseudo;
+
+public class NumberPair {
+    //------------------------------------- data structure
+    // two double numbers: firstNumber, secondNumber
+
+    // ------------------------------------ constructors (no-arg, all-fields)
+
+    // ------------------------------------- instance methods
+    // both return a double: calcSum, calcDifference
+
+    // common methods: toString, gets/sets
+
+    //------------------------------------- test
+    public static void main(String[] args) {
+        // create object
+        // print the object
+        // create variable - result
+        // get calcSum
+        // print result
+        // get calcDifference
+        // print result
+    }
+}
